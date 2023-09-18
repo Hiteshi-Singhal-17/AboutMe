401291e0b7b9ab770111cd03050ee91954abc517
singhh22 <singhh22@medtronic.com>
2023-09-18 22:56:51 +0530

    Finalized the project implementation.

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -4,17 +4,21 @@ import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val nameDetails = NameDetails("Jessica Singhal")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val doneButton = findViewById<Button>(R.id.done_button)
-        doneButton.setOnClickListener {
+        binding.nameDetails = nameDetails
+
+        binding.doneButton.setOnClickListener {
             updateNicknameDisplay(it as Button)
             hideKeyboard(it)
         }
@@ -22,15 +26,16 @@ class MainActivity : AppCompatActivity() {
 
 
     private fun updateNicknameDisplay(doneButton: Button) {
-        // Retrieve references to the necessary views.
-        val nicknameDisplay = findViewById<TextView>(R.id.nickname_text)
-        val nicknameInput = findViewById<EditText>(R.id.nickname_edit_text)
-
-        // Update the displayed nickname and adjust view visibilities.
-        nicknameDisplay.text = nicknameInput.text
-        nicknameInput.visibility = View.GONE
-        doneButton.visibility = View.GONE
-        nicknameDisplay.visibility = View.VISIBLE
+        binding.apply {
+            // Initializing nickname property of the nameDetails object
+            nameDetails = nameDetails?.copy(nickname = nicknameEdit.text.toString())
+            this.nameDetails = nameDetails
+//            invalidateAll()
+            // Update the displayed nickname and adjust view visibilities.
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
     }
 
 
